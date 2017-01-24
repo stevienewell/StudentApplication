186a4787e3aeb097eb1b8f06af27fa9c832757bb
stevienewell <snewell721@outlook.com>
2017-01-24 09:38:47 +0000

    Commit Student entity and .NET Core solution targetting 1.0.1

---
+++ StudentApplication/src/SA.Data/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace SA.Data.Entities
+{
+    public class Student : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string EnrollmentNo { get; set; }
+    }
+}
